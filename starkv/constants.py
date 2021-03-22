ede0171e9992c8c7df77e5b2a1891241f6fd3911
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-22 14:59:22 -0500

    Initial commit.

---
+++ starkv/constants.py
@@ -0,0 +1,26 @@
+
+UPDATE_INTERVAL = 1 / 60
+
+####
+# Colors
+####
+BACKGROUND_COLOR  =     0,     0,     0,   1
+
+NODE_COLOR        = 0.051, 0.278, 0.631,    1
+HIGHLIGHTED_NODE  = 0.758, 0.823,  0.92,    1
+
+EDGE_COLOR        =  0.160, 0.176, 0.467, 0.8
+HIGHLIGHTED_EDGE  =  0.760, 0.235, 0.239,   1
+
+####
+# Sizes
+####
+NODE_RADIUS   = 3
+BOUNDS        = NODE_RADIUS * 2
+
+NODE_WIDTH    = 3
+EDGE_WIDTH    = 2
+
+HEAD_SIZE     = 5  # size of arrow heads
+
+
