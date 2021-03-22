9f5ce3ace58a2919cefd9813d094d6fe7d5c48b4
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-22 15:03:20 -0500

    Remove reference to graph-tool attributes.  Store `is_pinned` boolean in the class itself.
    Import constants from constants.py.

--- starkv/node.py
+++ starkv/node.py
@@ -1,15 +1,18 @@
 from kivy.graphics import Color, Line
+from .constants import NODE_WIDTH, NODE_RADIUS, BOUNDS
 
 
 class Node(Line):
     __slots__ = 'color', 'vertex', 'canvas', 'group_name', 'list_item'
 
-    def __init__(self, vertex, canvas):
+    def __init__(self, vertex, canvas, color):
         self.group_name = str(id(self))
 
         self.vertex = vertex
         self.canvas = canvas
+        self.is_pinned = False
 
+        self._color = color
         self.color = Color(*color, group=self.group_name)
         super().__init__(width=NODE_WIDTH, group=self.group_name)
 
@@ -18,24 +21,24 @@ class Node(Line):
             self.canvas.edges[edge].update()
 
     def freeze(self, color=None):
-        self.canvas.G.vp.pinned[self.vertex] = 1
+        self.is_pinned = True
+
         if color is not None:
             self.color.rgba = color
 
         self.update_out_edges()
 
     def unfreeze(self):
-        canvas = self.canvas
-        canvas.G.vp.pinned[self.vertex] = 0
-        self.color.rgba = canvas.node_colormap[canvas.node_colors[self.vertex]]
+        self.is_pinned = False
+        self.color.rgba = self._color
         self.update_out_edges()
 
     def collides(self, mx, my):
-        x, y = self.canvas.coords[int(self.vertex)]
+        x, y = self.canvas.node_positions[self.vertex]
         return abs(x - mx) <= BOUNDS and abs(y - my) <= BOUNDS
 
     def update(self):
-        canvas = self.canvas
-        if not canvas.G.vp.pinned[self.vertex]:
-            self.color.rgba = canvas.node_colormap[canvas.node_colors[self.vertex]]
-        self.circle = *canvas.coords[int(self.vertex)], NODE_RADIUS
+        if not self.is_pinned:
+            self.color.rgba = self._color
+
+        self.circle = *self.canvas.node_positions[self.vertex], NODE_RADIUS
