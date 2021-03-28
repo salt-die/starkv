3ecb2646aab0b6d89ac564d64f9efdd0eb693ca7
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-28 00:22:54 -0500

    Made all color components floats.

--- starkv/constants.py
+++ starkv/constants.py
@@ -4,16 +4,16 @@ UPDATE_INTERVAL = 1 / 60
 ####
 # Colors
 ####
-BACKGROUND_COLOR  =     0,     0,     0,    1
+BACKGROUND_COLOR  =   0.0,   0.0,   0.0, 1.0
 
-NODE_COLOR        = 0.051, 0.278, 0.631,    1
-HIGHLIGHTED_NODE  = 0.758, 0.823,  0.92,    1
+NODE_COLOR        = 0.051, 0.278, 0.631, 1.0
+HIGHLIGHTED_NODE  = 0.758, 0.823,  0.92, 1.0
 
-EDGE_COLOR        = 0.160, 0.176, 0.467,  0.8
-HIGHLIGHTED_EDGE  = 0.760, 0.235, 0.239,    1
+EDGE_COLOR        = 0.160, 0.176, 0.467, 0.8
+HIGHLIGHTED_EDGE  = 0.760, 0.235, 0.239, 1.0
 
 HEAD_COLOR        = 0.192, 0.211, 0.560, 0.96
-HIGHLIGHTED_HEAD  = 0.912, 0.282, 0.287,    1
+HIGHLIGHTED_HEAD  = 0.912, 0.282, 0.287, 1.0
 
 ####
 # Sizes
@@ -25,5 +25,3 @@ NODE_WIDTH    = 3
 EDGE_WIDTH    = 2
 
 HEAD_SIZE     = 5  # size of arrow heads
-
-
