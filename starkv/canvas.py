3913d221778521980d77cfc62a6cf4f12c4d8e11
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-28 01:28:57 -0500

    node_mass is default value, so doesn't need to be specified.

--- starkv/canvas.py
+++ starkv/canvas.py
@@ -197,7 +197,7 @@ class GraphCanvas(Widget):
             edge.update()
 
     def step_layout(self, dt):
-        self._unscaled_layout = self.G.layout_graphopt(niter=1, seed=self._unscaled_layout, node_mass=30, max_sa_movement=.1, node_charge=.00001)
+        self._unscaled_layout = self.G.layout_graphopt(niter=1, seed=self._unscaled_layout, max_sa_movement=.1, node_charge=.00001)
 
         if self.highlighted is not None:  # Keep highlighted node from moving by reseting it's position after updating layout
             self._unscaled_layout[self.highlighted.vertex.index] = self.highlighted.is_pinned
