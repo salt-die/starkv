2d1612e54bb75a98e85cda6e2948897f88959adf
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-27 19:37:14 -0500

    HEAD_COLOR, HIGHLIGHTED_HEAD constants added.

--- starkv/constants.py
+++ starkv/constants.py
@@ -4,13 +4,16 @@ UPDATE_INTERVAL = 1 / 60
 ####
 # Colors
 ####
-BACKGROUND_COLOR  =     0,     0,     0,   1
+BACKGROUND_COLOR  =     0,     0,     0,    1
 
 NODE_COLOR        = 0.051, 0.278, 0.631,    1
 HIGHLIGHTED_NODE  = 0.758, 0.823,  0.92,    1
 
-EDGE_COLOR        =  0.160, 0.176, 0.467, 0.8
-HIGHLIGHTED_EDGE  =  0.760, 0.235, 0.239,   1
+EDGE_COLOR        = 0.160, 0.176, 0.467,  0.8
+HIGHLIGHTED_EDGE  = 0.760, 0.235, 0.239,    1
+
+HEAD_COLOR        = 0.192, 0.211, 0.560, 0.96
+HIGHLIGHTED_HEAD  = 0.912, 0.282, 0.287,    1
 
 ####
 # Sizes
