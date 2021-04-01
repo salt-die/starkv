b68ae611e2bb612713bb67020a7cb548c2d74be6
salt-die <53280662+salt-die@users.noreply.github.com>
2021-03-31 20:04:19 -0500

    All methods and properties relating to freezing removed.  `Edge` will handle coloring the Node if the edge is selected.
    AnimatedNode class added.

--- starkv/node.py
+++ starkv/node.py
@@ -1,36 +1,40 @@
-from kivy.graphics import Color, Line
+from math import tau
+from pathlib import Path
+
+from kivy.graphics import Color, Line, Rectangle
+from kivy.core.image import Image
+
 from .constants import (
     NODE_WIDTH,
     NODE_RADIUS,
     NODE_COLOR,
     HIGHLIGHTED_NODE,
+    HIGHLIGHTED_EDGE,
 )
+
+STAR = Image(str(Path("starkv") / "assets" / "star.png"))
 
 
 class Node(Line):
-    __slots__ = 'color', 'vertex', 'canvas', 'group_name', '_x', '_y'
+    __slots__ = 'color', 'index', 'canvas',
 
-    def __init__(self, vertex, canvas):
-        self.group_name = str(id(self))
-        self.vertex = vertex
+    def __init__(self, index, canvas):
+        self.index = index
         self.canvas = canvas
-        self.color = Color(*NODE_COLOR, group=self.group_name)
-        self._x = self._y = None
-        super().__init__(width=NODE_WIDTH, group=self.group_name)
+        self.color = Color(*NODE_COLOR)
 
-    @property
-    def is_frozen(self):
-        return self._x is not None is not self._y
-
-    def freeze(self):
-        self._x, self._y = self.canvas._unscaled_layout[self.vertex.index]
-        self.color.rgba = HIGHLIGHTED_NODE
-
-    def unfreeze(self):
-        self._x = self._y = None
-        self.color.rgba = NODE_COLOR
+        super().__init__(width=NODE_WIDTH)
 
     def update(self):
-        if self.is_frozen:
-            self.canvas._unscaled_layout[self.vertex.index] = self._x, self._y
-        self.circle = *self.canvas.layout[self.vertex.index], NODE_RADIUS
+        self.circle = *self.canvas.layout[self.index], NODE_RADIUS
+
+
+class AnimatedNode(Rectangle):
+    __slots__ = 'color',
+
+    def __init__(self, *args, **kwargs):
+        self.color = Color(*HIGHLIGHTED_EDGE)
+        self.color.a = 0
+
+        super().__init__(*args, **kwargs)
+        self.texture = STAR.texture
