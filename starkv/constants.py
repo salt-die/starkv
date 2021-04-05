19faac97db1e34909d91ced6b4438bd21535da38
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-05 07:56:48 -0500

    Minimum scale added.

--- starkv/constants.py
+++ starkv/constants.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 UPDATE_INTERVAL = 1 / 60
 TOUCH_INTERVAL = .4  # Number of seconds before a touch event is considered a touch move event.
+MIN_SCALE = .05
 
 # Colors
 BACKGROUND_COLOR  =   0.0,   0.0,   0.0, 1.0
