cd03fbbd5699d26c9fd08d74064da1c91f2909b2
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-13 09:05:31 -0500

    `UNIT` renamed to `WHITE`. `Arrow`  class removed.  `Edge` has taken responsibility of `Triangle` instruction. Added an `update_points` method to `Edge` that will set the edge's points and also update the triangle's points.

--- starkv/edge.py
+++ starkv/edge.py
@@ -3,8 +3,8 @@ from math import atan2, cos, hypot, sin
 from kivy.graphics import Color, Line, Triangle
 
 from .constants import (
-    BASE,
-    UNIT,
+    HEAD_BASE,
+    WHITE,
     EDGE_BOUNDS,
     EDGE_WIDTH,
     HEAD_SIZE,
@@ -19,58 +19,21 @@ from .constants import (
 )
 
 
-class Arrow(Triangle):
-    __slots__ = 'base', 'color'
-
-    def __init__(self, color, size, group=None):
-        """
-        Triangle points are: (-0.5, 0), (-4, 1), (-4, -1). Looks like:
-        (Two characters per x unit, One line per y unit, O is origin)
-
-                        |
-               o        |
-            ----------o-O---
-               o        |
-                        |
-
-        Tip is off origin so that arrow is less covered by nodes.
-        """
-        self.base =  tuple(x * size for x in BASE)
-        group = group or str(id(self))
-
-        self.color = Color(*color, group=group)
-        super().__init__(group=group, points=(0, 0, 0, 0, 0, 0))
-
-    def update(self, x1, y1, x2, y2):
-        theta = atan2(y2 - y1, x2 - x1)
-
-        cosine = cos(theta)
-        sine = sin(theta)
-
-        bx1, by1, bx2, by2, bx3, by3 = self.base
-        self.points = (
-            cosine * bx1 + by1 *  -sine + x2,
-            sine   * bx1 + by1 * cosine + y2,
-            cosine * bx2 + by2 *  -sine + x2,
-            sine   * bx2 + by2 * cosine + y2,
-            cosine * bx3 + by3 *  -sine + x2,
-            sine   * bx3 + by3 * cosine + y2,
-        )
-
-
 class Edge(Line):
-    __slots__ = "edge", "canvas", "_is_tail_selected", "color", 'head'
+    __slots__ = "edge", "canvas", "_is_tail_selected", "color", 'head_color', 'head'
+
+    HEAD = tuple(x * HEAD_SIZE for x in HEAD_BASE)
 
     def __init__(self, edge, canvas):
-        group = str(id(self))
-
         self.edge = edge
         self.canvas = canvas
         self._is_tail_selected = None
-        self.color = Color(*EDGE_COLOR, group=group)
+        self.color = Color(*EDGE_COLOR)
 
-        super().__init__(width=EDGE_WIDTH, group=group)
-        self.head = Arrow(color=HEAD_COLOR, size=HEAD_SIZE, group=group)
+        super().__init__(width=EDGE_WIDTH)
+
+        self.head_color = Color(*HEAD_COLOR)
+        self.head = Triangle(points=(0, 0, 0, 0, 0, 0))
 
     @property
     def is_tail_selected(self):
@@ -95,11 +58,11 @@ class Edge(Line):
             for edge in G.vs[unfrozen.index].out_edges():
                 e = edges[edge.tuple]
                 e.color.rgba = EDGE_COLOR
-                e.head.color.rgba = HEAD_COLOR
+                e.head_color.rgba = HEAD_COLOR
 
         if value is not None:
-            self.color.rgba = UNIT
-            self.head.color.rgba, frozen = (HEAD_COLOR, nodes[source]) if value else (HIGHLIGHTED_HEAD, nodes[target])
+            self.color.rgba = WHITE
+            self.head_color.rgba, frozen = (HEAD_COLOR, nodes[source]) if value else (HIGHLIGHTED_HEAD, nodes[target])
             self.canvas.selected_node = frozen
             frozen.color.rgba = HIGHLIGHTED_NODE
 
@@ -107,11 +70,11 @@ class Edge(Line):
                 e = edges[edge.tuple]
                 if e is not self:
                     e.color.rgba = HIGHLIGHTED_EDGE
-                    e.head.color.rgba = HIGHLIGHTED_HEAD
+                    e.head_color.rgba = HIGHLIGHTED_HEAD
 
         else:
             self.color.rgba = EDGE_COLOR
-            self.head.color.rgba = HEAD_COLOR
+            self.head_color.rgba = HEAD_COLOR
             self.canvas.selected_node = None
 
         self._is_tail_selected = value
@@ -122,24 +85,32 @@ class Edge(Line):
         layout = self.canvas.layout
         return *layout[source], *layout[target],
 
-    def update(self):
-        x1, y1, x2, y2 = self.layout_points
+    def update_points(self, x1, y1, x2, y2):
+        theta = atan2(y2 - y1, x2 - x1)
+        cosine = cos(theta)
+        sine = sin(theta)
+        bx1, by1, bx2, by2, bx3, by3 = Edge.HEAD
 
         self.points = x1, y1, x2, y2
-        self.head.update(x1, y1, x2, y2)
+        self.head.points = (
+            cosine * bx1 + by1 *  -sine + x2,
+            sine   * bx1 + by1 * cosine + y2,
+            cosine * bx2 + by2 *  -sine + x2,
+            sine   * bx2 + by2 * cosine + y2,
+            cosine * bx3 + by3 *  -sine + x2,
+            sine   * bx3 + by3 * cosine + y2,
+        )
 
-        if self.is_tail_selected is None:
-            return
+    def update(self):
+        self.update_points(*self.layout_points)
 
-        if self.is_tail_selected:
-            self.texture = SELECTED_GRADIENT
-        else:
-            self.texture = SELECTED_GRADIENT_REVERSED
+        if self.is_tail_selected is not None:
+            self.texture = SELECTED_GRADIENT if self.is_tail_selected else SELECTED_GRADIENT_REVERSED
 
     def collides(self, px, py):
         """
-        Returns a 2-tuple (is_close, closer_to_tail), where `is_close` indicates if `(px, py)` is within
-        `EDGE_BOUNDS` and `closer_to_tail` indicates if the point is closer to the tail or the head.
+        Returns a 2-tuple (is_close, is_closer_to_tail), where `is_close` indicates if `(px, py)` is within
+        `EDGE_BOUNDS` of this edge and `is_closer_to_tail` indicates if the point is closer to the tail or the head.
         """
         ax, ay, bx, by = self.layout_points
 
