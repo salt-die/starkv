9de7d8be74fb48ea61610b0c92b7a8d20da94177
salt-die <53280662+salt-die@users.noreply.github.com>
2021-04-17 20:43:13 -0500

    GraphCanvas pass to constructor so that we can set attributes on dismiss.

--- starkv/popup.py
+++ starkv/popup.py
@@ -1,9 +1,19 @@
 from kivy.uix.popup import Popup
 from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 
 
 class NewGameDialogue(Popup):
-    ...
+    graph_canvas = ObjectProperty()
+
+    def __init__(self, graph_canvas):
+        self.graph_canvas = graph_canvas
+        super().__init__()
+
+    def accept(self):
+        self.dismiss()
+        self.graph_canvas.nnodes = int(self.ids['slider'].value)
+        self.graph_canvas.setup_canvas()
 
 
 Builder.load_string("""
@@ -26,6 +36,6 @@ Builder.load_string("""
 
         Button:
             text: "Accept"
-            on_release: root.dismiss()
+            on_release: root.accept()
 """
 )
